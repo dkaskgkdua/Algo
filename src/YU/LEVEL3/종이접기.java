1afe1e8d4832e07bcba912eae1746d53e699f5de
yuzi0 <zag001@naver.com>
2020-04-22 22:56:02 +0900

    2020-04-18 종기접기 성공 코드

--- src/YU/LEVEL3/종이접기.java
+++ src/YU/LEVEL3/종이접기.java
@@ -1,27 +1,29 @@
 // https://programmers.co.kr/learn/courses/30/lessons/62049
-// 테스트 케이스 17번만 실패
+// 통과
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 class Solution {
 
 	public int[] solution(int n) {
-		List<Integer> answerList = new LinkedList<>();
-		int[] routine = { 0, 0, 0, 1 };
-		answerList.add(0);
-
+        StringBuilder answerList = new StringBuilder();
+		String[] routine = { "0", "0", "1", "0" };
+        int max = 1;
+        
+        // answer
+		answerList.append(0);
 		for (int round = 2; round <= n; round++) {
-			List<Integer> tmp = new ArrayList<>();
-			for (int i = 1; i < Math.pow(2, round); i++) {
-				tmp.add((i % 2 == 0)? answerList.remove(0) : routine[i % 4]);
+            max = max * 2 + 1;
+			for (int i = 0; i < max; i += 2) {
+				answerList.insert(i, (i % 2 != 0)? answerList.charAt(i-1) : routine[i % 4]);
 			}
-			answerList = tmp;
 		}
-
-		int[] answer = new int[answerList.size()];
-		for (int i = 0; i < answerList.size(); i++) {
-			answer[i] = answerList.get(i);
+        
+        // split
+		int[] answer = new int[answerList.length()];
+		for (int i = 0; i < answerList.length(); i++) {
+			answer[i] = answerList.charAt(i) - '0';
 		}
 
 		return answer;
